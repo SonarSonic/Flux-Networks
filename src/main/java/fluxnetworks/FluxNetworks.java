d37e91f64ed126140fd710ed3f4be70230cfeab1
BloCamLimb <pocamelards@gmail.com>
2019-09-23 19:47:18 +0800

    update localization

--- src/main/java/fluxnetworks/FluxNetworks.java
+++ src/main/java/fluxnetworks/FluxNetworks.java
@@ -13,7 +13,7 @@ public class FluxNetworks {
 
     public static final String MODID = "fluxnetworks";
     public static final String NAME = "Flux Networks";
-    public static final String VERSION = "4.0.9";
+    public static final String VERSION = "4.0.10";
 
     @Mod.Instance(MODID)
     public static FluxNetworks instance;
