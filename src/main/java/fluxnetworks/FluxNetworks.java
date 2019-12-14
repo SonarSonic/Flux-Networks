b9d4a9214f3c49c82a3065cb5331e9330b7d8f28
BloCamLimb <pocamelards@gmail.com>
2019-12-14 12:58:34 +0800

    Update version number

--- src/main/java/fluxnetworks/FluxNetworks.java
+++ src/main/java/fluxnetworks/FluxNetworks.java
@@ -12,7 +12,7 @@ public class FluxNetworks {
 
     public static final String MODID = "fluxnetworks";
     public static final String NAME = "Flux Networks";
-    public static final String VERSION = "4.0.13";
+    public static final String VERSION = "4.0.14";
 
     @Mod.Instance(MODID)
     public static FluxNetworks instance;
