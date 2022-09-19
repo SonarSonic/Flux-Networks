752c4773f55b12db847dbde35de03784ed27fd43
Quinteger <counterdisclaimer@gmail.com>
2022-09-19 09:44:30 +0300

    A bit of item registration refactoring

--- src/main/java/sonar/fluxnetworks/register/RegistryItems.java
+++ src/main/java/sonar/fluxnetworks/register/RegistryItems.java
@@ -32,13 +32,13 @@ public class RegistryItems {
     private static final Properties DEVICE_PROPS = new Properties().tab(CREATIVE_MODE_TAB).fireResistant().stacksTo(1);
 
 
-    public static final RegistryObject<BlockItem> FLUX_BLOCK = registerBlockItem("flux_block", RegistryBlocks.FLUX_BLOCK, BlockItem::new);
-    public static final RegistryObject<FluxDeviceItem> FLUX_PLUG = registerBlockItem("flux_plug", RegistryBlocks.FLUX_PLUG, FluxDeviceItem::new);
-    public static final RegistryObject<FluxDeviceItem> FLUX_POINT = registerBlockItem("flux_point", RegistryBlocks.FLUX_POINT, FluxDeviceItem::new);
-    public static final RegistryObject<FluxDeviceItem> FLUX_CONTROLLER = registerBlockItem("flux_controller", RegistryBlocks.FLUX_CONTROLLER, FluxDeviceItem::new);
-    public static final RegistryObject<FluxStorageItem> BASIC_FLUX_STORAGE = registerBlockItem("basic_flux_storage", RegistryBlocks.BASIC_FLUX_STORAGE, FluxStorageItem::new);
-    public static final RegistryObject<FluxStorageItem> HERCULEAN_FLUX_STORAGE = registerBlockItem("herculean_flux_storage", RegistryBlocks.HERCULEAN_FLUX_STORAGE, FluxStorageItem::new);
-    public static final RegistryObject<FluxStorageItem> GARGANTUAN_FLUX_STORAGE = registerBlockItem("gargantuan_flux_storage", RegistryBlocks.GARGANTUAN_FLUX_STORAGE, FluxStorageItem::new);
+    public static final RegistryObject<BlockItem> FLUX_BLOCK = registerBlockItem("flux_block", NORMAL_PROPS, RegistryBlocks.FLUX_BLOCK, BlockItem::new);
+    public static final RegistryObject<FluxDeviceItem> FLUX_PLUG = registerBlockItem("flux_plug", NORMAL_PROPS, RegistryBlocks.FLUX_PLUG, FluxDeviceItem::new);
+    public static final RegistryObject<FluxDeviceItem> FLUX_POINT = registerBlockItem("flux_point", NORMAL_PROPS, RegistryBlocks.FLUX_POINT, FluxDeviceItem::new);
+    public static final RegistryObject<FluxDeviceItem> FLUX_CONTROLLER = registerBlockItem("flux_controller", NORMAL_PROPS, RegistryBlocks.FLUX_CONTROLLER, FluxDeviceItem::new);
+    public static final RegistryObject<FluxStorageItem> BASIC_FLUX_STORAGE = registerBlockItem("basic_flux_storage", NORMAL_PROPS, RegistryBlocks.BASIC_FLUX_STORAGE, FluxStorageItem::new);
+    public static final RegistryObject<FluxStorageItem> HERCULEAN_FLUX_STORAGE = registerBlockItem("herculean_flux_storage", NORMAL_PROPS, RegistryBlocks.HERCULEAN_FLUX_STORAGE, FluxStorageItem::new);
+    public static final RegistryObject<FluxStorageItem> GARGANTUAN_FLUX_STORAGE = registerBlockItem("gargantuan_flux_storage", NORMAL_PROPS, RegistryBlocks.GARGANTUAN_FLUX_STORAGE, FluxStorageItem::new);
 
     public static final RegistryObject<FluxDustItem> FLUX_DUST = registerItem("flux_dust", NORMAL_PROPS, FluxDustItem::new);
     public static final RegistryObject<Item> FLUX_CORE = registerItem("flux_core", NORMAL_PROPS, Item::new);
@@ -46,11 +46,11 @@ public class RegistryItems {
     public static final RegistryObject<ItemFluxConfigurator> FLUX_CONFIGURATOR = registerItem("flux_configurator", DEVICE_PROPS, ItemFluxConfigurator::new);
     public static final RegistryObject<ItemAdminConfigurator> ADMIN_CONFIGURATOR = registerItem("admin_configurator", DEVICE_PROPS, ItemAdminConfigurator::new);
 
-    private static <I extends Item, B extends Block> RegistryObject<I> registerBlockItem(String name, RegistryObject<B> blockRegistryObject, BiFunction<? super B, ? super Properties, I> factory) {
-        return registerItem(name, NORMAL_PROPS, (props) -> factory.apply(blockRegistryObject.get(), props));
+    private static <I extends Item, B extends Block> RegistryObject<I> registerBlockItem(String name, Properties itemProperties, RegistryObject<B> blockRegistryObject, BiFunction<? super B, ? super Properties, ? extends I> factory) {
+        return registerItem(name, itemProperties, (props) -> factory.apply(blockRegistryObject.get(), props));
     }
 
-    private static <I extends Item> RegistryObject<I> registerItem(String name, Properties itemProperties, Function<? super Properties, I> factory) {
+    private static <I extends Item> RegistryObject<I> registerItem(String name, Properties itemProperties, Function<? super Properties, ? extends I> factory) {
         return REGISTRY.register(name, () -> factory.apply(itemProperties));
     }
 
